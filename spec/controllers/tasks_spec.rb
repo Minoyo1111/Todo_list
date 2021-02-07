26d9fc61a14de0132edb672f7438d2937bec2373
mino <oil_chen@cycu.org.tw>
2021-02-07 11:50:27 +0800

    controller 測試

--- spec/controllers/tasks_spec.rb
+++ spec/controllers/tasks_spec.rb
@@ -1,4 +1,8 @@
 require 'rails_helper'
+before(:all) do 
+  @task_1 = Task.create(title: "title_1", description: "description_1")
+  @task_2 = Task.create(title: "title_2", description: "description_2")
+end
 
 RSpec.describe TasksController do
   describe "#index" do
@@ -7,6 +11,24 @@ RSpec.describe TasksController do
 
       expect(response).to render_template(:index)
       expect(response).to have_http_status(200)
+    end
+  end
+
+  describe "#new" do
+    it "response is success" do
+      get :new
+
+      expect(response).to have_http_status(200)
+      expect(response).to render_template(:new)
+    end
+  end
+
+  describe "#edit" do
+    it "response is success" do
+      get :edit, id: @task_1[:id]
+
+      expect(response).to have_http_status(200)
+      expect(response).to render_template(:edit)
     end
   end
 end
