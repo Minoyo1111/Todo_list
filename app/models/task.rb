2e84cebee9c5e3196285010ecd4eb2387502e6db
mino <oil_chen@cycu.org.tw>
2021-02-08 16:08:25 +0800

    model_rspec

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,13 @@
 class Task < ApplicationRecord
+  belongs_to :user
   validates :title, presence: true
+
+  def self.no_description
+    where(description: nil)
+  end
+
+  def abstract
+    self.description[0..20]
+  end
+
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -3,5 +3,20 @@ require 'rails_helper'
 RSpec.describe Task, type: :model do
   it "validate title" do
     expect(Task.new).not_to be_valid
+    expect(Task.new(title: "title")).to be_valid
   end
+
+  it ".no_description" do
+    task_with_description = Task.create(title: "title", description: "description")
+    task_without_description = Task.create(title: "title", description: nil)
+    expect(Task.no_description).to include task_without_description
+    expect(Task.no_description).not_to include task_with_description
+  end
+
+  it "#abstract" do 
+    task = Task.create(title: "title", description: "12345678912345678900yaya")
+    expect(task.abstract).to include("12345678912345678900")
+    expect(task.abstract).not_to include("yaya")
+  end
+
 end
