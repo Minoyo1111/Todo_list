687be3a6c0a5c35f556623f3eb97bb20330b1a4d
mino <oil_chen@cycu.org.tw>
2021-02-09 12:02:17 +0800

    test basic finish

--- spec/requests/tasks_spec.rb
+++ spec/requests/tasks_spec.rb
@@ -12,11 +12,19 @@ RSpec.describe "tasks", type: :request do
     expect(response.body).to include("task from require spec")
   end
   it "#show" do
-  get "/tasks/#{@task.id}"
-  expect(response).to have_http_status(200)
-  expect(response).to ren der_template(:show)
-  expect(response.body).to include("任務標題")
-  expect(response.body).to include("task from require spec") 
+    get "/tasks/#{@task.id}"
+    expect(response).to have_http_status(200)
+    expect(response).to render_template(:show)
+    expect(response.body).to include("任務標題")
+    expect(response.body).to include("task from require spec") 
+  end
+
+  it "#create" do
+    params = {title: "create title", description: "create discription"}
+    post "/tasks", params:{ task: params}
+    #會轉址不會是200
+    expect(response).to have_http_status(302)
+    expect(Task.last.title).to eq(params[:title])
   end
 
 end
