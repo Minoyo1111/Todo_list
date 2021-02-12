0014933f818b63035dcfc7a9e827a095b1a1c46a
mino <oil_chen@cycu.org.tw>
2021-02-12 11:03:53 +0800

    fix path & don't use method

--- spec/requests/tasks_spec.rb
+++ spec/requests/tasks_spec.rb
@@ -5,13 +5,13 @@ RSpec.describe "tasks", type: :request do
     @task = Task.create(title: "task from require spec")
   end
   it "#index" do
-    get "/tasks"
+    get tasks_path
     expect(response).to have_http_status(200)
     expect(response).to render_template(:index)
     expect(response.body).to include("task from require spec")
   end
   it "#show" do
-    get "/tasks/#{@task.id}"
+    get task_path(id: @task.id)
     expect(response).to have_http_status(200)
     expect(response).to render_template(:show)
     expect(response.body).to include("task from require spec") 
@@ -19,7 +19,7 @@ RSpec.describe "tasks", type: :request do
 
   it "#create" do
     params = {title: "create title", description: "create discription"}
-    post "/tasks", params:{ task: params}
+    put task_path(id: @task.id), params:{ task: params}
     #會轉址不會是200
     expect(response).to have_http_status(302)
     expect(Task.last.title).to eq(params[:title])
