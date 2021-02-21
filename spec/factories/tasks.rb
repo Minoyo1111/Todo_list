90d83785d2e2c390e3ea3cc7183753a4be7d2293
mino <oil_chen@cycu.org.tw>
2021-02-21 13:35:41 +0800

    done

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,7 +1,6 @@
 FactoryBot.define do
   factory :task do
-    title "task from require spec"
+    title { "task from require spec" }
     description { Faker::Lorem.paragraph }
-    online { true }
   end
 end
