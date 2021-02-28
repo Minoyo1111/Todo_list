f449982a4506290868fb06a9a7a0ed057f3400b8
mino <oil_chen@cycu.org.tw>
2021-02-28 12:34:13 +0800

    finish

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_31_064532) do
+ActiveRecord::Schema.define(version: 2021_02_28_040322) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 2021_01_31_064532) do
   end
 
   create_table "tasks", force: :cascade do |t|
-    t.string "title"
+    t.string "title", null: false
     t.text "description"
     t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
